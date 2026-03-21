42acee8a7221ea6a1ad7cffd02af7cc47e5af708
Trần Hạc Hương Thư <thuthh234112e@st.uel.edu.vn>
2026-03-21 16:05:40 +0700

    commnit collection

--- my-app/src/app/collection/collection.ts
+++ my-app/src/app/collection/collection.ts
@@ -22,7 +22,11 @@ export class Collection implements OnInit {
         'assets/kimchingocdiep1.png',
         'assets/kimchingocdiep2.png',
         'assets/kimchingocdiep3.png',
-        'assets/kimchingocdiep4.png'
+        'assets/kimchingocdiep4.png',
+        'assets/kimchingocdiep5.png',
+        'assets/kimchingocdiep6.png',
+        'assets/kimchingocdiep7.png',
+        'assets/kimchingocdiep8.png'
       ]
     },
     'nhuoc-cam-phu-hoa': {
@@ -31,7 +35,11 @@ export class Collection implements OnInit {
         'assets/nhuoccamphuhoa1.png',
         'assets/nhuoccamphuhoa2.png',
         'assets/nhuoccamphuhoa3.png',
-        'assets/nhuoccamphuhoa4.png'
+        'assets/nhuoccamphuhoa4.png',
+        'assets/nhuoccamphuhoa5.png',
+        'assets/nhuoccamphuhoa6.png',
+        'assets/nhuoccamphuhoa7.png',
+        'assets/nhuoccamphuhoa8.png'
       ]
     },
     'thuy-tuc-uyen-tam': {
@@ -40,7 +48,11 @@ export class Collection implements OnInit {
         'assets/thuytucuyentam1.png',
         'assets/thuytucuyentam2.png',
         'assets/thuytucuyentam3.png',
-        'assets/thuytucuyentam4.png'
+        'assets/thuytucuyentam4.png',
+        'assets/thuytucuyentam5.png',
+        'assets/thuytucuyentam6.png',
+        'assets/thuytucuyentam7.png',
+        'assets/thuytucuyentam8.png'
       ]
     },
     'y-hien': {
@@ -49,24 +61,14 @@ export class Collection implements OnInit {
         'assets/yhien1.png',
         'assets/yhien2.png',
         'assets/yhien3.png',
-        'assets/yhien4.png'
+        'assets/yhien4.png',
+        'assets/yhien5.png',
+        'assets/yhien6.png',
+        'assets/yhien7.png',
+        'assets/yhien8.png'
       ]
     }
   };
-
-
-  constructor(private route: ActivatedRoute) {}
-
-
-  ngOnInit() {
-    this.route.params.subscribe(params => {
-      const id = params['id'] || params['collectionName'];
-      if (this.data[id]) {
-        this.collectionTitle = this.data[id].title;
-        this.album = this.data[id].images;
-      }
-    });
-  }
 
 
   @HostListener('window:scroll', ['$event'])
@@ -75,12 +77,24 @@ export class Collection implements OnInit {
     sections.forEach((section: any) => {
       const bg = section.querySelector('.parallax-bg');
       if (bg) {
-        const speed = 0.4;
+        const speed = 0.5;
         const rect = section.getBoundingClientRect();
         if (rect.top < window.innerHeight && rect.bottom > 0) {
           const shift = rect.top * speed;
           bg.style.transform = `translateY(${shift}px)`;
         }
+      }
+    });
+  }
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      const id = params['id'] || params['collectionName'];
+      if (this.data[id]) {
+        this.collectionTitle = this.data[id].title;
+        this.album = this.data[id].images;
       }
     });
   }
