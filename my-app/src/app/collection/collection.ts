d8bd39897c5ab58f24a246576c25416d1b757671
Trần Hạc Hương Thư <thuthh234112e@st.uel.edu.vn>
2026-03-21 16:51:00 +0700

    commit collection

--- my-app/src/app/collection/collection.ts
+++ my-app/src/app/collection/collection.ts
@@ -19,53 +19,53 @@ export class Collection implements OnInit {
     'kim-chi-ngoc-diep': {
       title: 'Kim Chi Ngọc Diệp',
       images: [
-        'assets/kimchingocdiep1.png',
-        'assets/kimchingocdiep2.png',
-        'assets/kimchingocdiep3.png',
-        'assets/kimchingocdiep4.png',
-        'assets/kimchingocdiep5.png',
-        'assets/kimchingocdiep6.png',
-        'assets/kimchingocdiep7.png',
-        'assets/kimchingocdiep8.png'
+        '/assets/kimchingocdiep1.png',
+        '/assets/kimchingocdiep2.png',
+        '/assets/kimchingocdiep3.png',
+        '/assets/kimchingocdiep4.png',
+        '/assets/kimchingocdiep5.png',
+        '/assets/kimchingocdiep6.png',
+        '/assets/kimchingocdiep7.png',
+        '/assets/kimchingocdiep8.png'
       ]
     },
     'nhuoc-cam-phu-hoa': {
-      title: 'Nhược Cầm Phù Hoa',
+      title: 'Nhược Cẩm Phù Hoa',
       images: [
-        'assets/nhuoccamphuhoa1.png',
-        'assets/nhuoccamphuhoa2.png',
-        'assets/nhuoccamphuhoa3.png',
-        'assets/nhuoccamphuhoa4.png',
-        'assets/nhuoccamphuhoa5.png',
-        'assets/nhuoccamphuhoa6.png',
-        'assets/nhuoccamphuhoa7.png',
-        'assets/nhuoccamphuhoa8.png'
+        '/assets/nhuoccamphuhoa1.png',
+        '/assets/nhuoccamphuhoa2.png',
+        '/assets/nhuoccamphuhoa3.png',
+        '/assets/nhuoccamphuhoa4.png',
+        '/assets/nhuoccamphuhoa5.png',
+        '/assets/nhuoccamphuhoa6.png',
+        '/assets/nhuoccamphuhoa7.png',
+        '/assets/nhuoccamphuhoa8.png'
       ]
     },
     'thuy-tuc-uyen-tam': {
       title: 'Thuỷ Túc Uyên Tầm',
       images: [
-        'assets/thuytucuyentam1.png',
-        'assets/thuytucuyentam2.png',
-        'assets/thuytucuyentam3.png',
-        'assets/thuytucuyentam4.png',
-        'assets/thuytucuyentam5.png',
-        'assets/thuytucuyentam6.png',
-        'assets/thuytucuyentam7.png',
-        'assets/thuytucuyentam8.png'
+        '/assets/thuytucuyentam1.png',
+        '/assets/thuytucuyentam2.png',
+        '/assets/thuytucuyentam3.png',
+        '/assets/thuytucuyentam4.png',
+        '/assets/thuytucuyentam5.png',
+        '/assets/thuytucuyentam6.png',
+        '/assets/thuytucuyentam7.png',
+        '/assets/thuytucuyentam8.png'
       ]
     },
     'y-hien': {
       title: 'Ý Hiên',
       images: [
-        'assets/yhien1.png',
-        'assets/yhien2.png',
-        'assets/yhien3.png',
-        'assets/yhien4.png',
-        'assets/yhien5.png',
-        'assets/yhien6.png',
-        'assets/yhien7.png',
-        'assets/yhien8.png'
+        'public/assets/yhien1.png',
+        'public/assets/yhien2.png',
+        'public/assets/yhien3.png',
+        'public/assets/yhien4.png',
+        'public/assets/yhien5.png',
+        'public/assets/yhien6.png',
+        'public/assets/yhien7.png',
+        'public/assets/yhien8.png'
       ]
     }
   };
